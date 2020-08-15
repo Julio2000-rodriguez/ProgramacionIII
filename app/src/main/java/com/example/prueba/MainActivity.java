2b1143732ab29c92942d4f64a146f82c716510a2
Julio2000-rodriguez <jarp69721@gmail.com>
2020-08-14 23:28:19 -0600

    4 controles agregados

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -49,25 +49,82 @@ public class MainActivity extends Activity {
                 case R.id.optMultiplicar:
                     respuesta = num1 * num2;
                     break;
+
                 case R.id.optDivision:
                     respuesta = num1 / num2;
+                    break;
+
+                case R.id.optporcentaje:
+                    respuesta = (num1 * num2) / 100;
+                    break;
+
+                case R.id.optexpo:
+                    respuesta = Math.pow(num1,num2);
+                    break;
+
+                case R.id.optmodulo:
+                    respuesta = num1 % num2;
+                    break;
+
+                case R.id.optfactoreal:
+
+                    int facto = 1;
+
+                    for (int i = 2; i<= num1; i++)
+
+                    {
+                        facto = facto * i;
+
+                        respuesta = facto;
+                    }
                     break;
             }
             //Este es para el spinner... -> Combobox.
             switch (cboOperaciones.getSelectedItemPosition()){
+
                 case 1: //suma
                     respuesta = num1 + num2;
                     break;
+
                 case 2: //resta
                     respuesta = num1 - num2;
                     break;
+
                 case 3: //multiplicacion
                     respuesta = num1 * num2;
                     break;
+
                 case 4: //division
                     respuesta = num1 / num2;
                     break;
+
+                case 5: //porcentaje
+                    respuesta = (num1 * num2) / 100;
+                    break;
+
+                case 6: //exponenciacion
+                    respuesta = Math.pow(num1,num2);
+                    break;
+
+                case 7: //modulo
+                    respuesta = num1 % num2;
+                    break;
+
+                case 8:
+
+                    int Facto = 1;
+
+                    for (int x = 2; x <= num1; x++)
+
+                    {
+                        Facto = Facto * x;
+
+                        respuesta = Facto;
+                    }
+
+                    break;
             }
+
             tempVal = (TextView) findViewById(R.id.lblRespuesta);
             tempVal.setText("Respuesta: " + respuesta);
         }catch (Exception err){
